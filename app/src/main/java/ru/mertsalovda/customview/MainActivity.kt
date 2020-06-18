4b8622175c180d746b71a6ab4033aedfc5314218
Korran <korranhorn009@gmail.com>
2020-06-18 04:22:29 +0300

    ProgressBarView Animated progress

--- app/src/main/java/ru/mertsalovda/customview/MainActivity.kt
+++ app/src/main/java/ru/mertsalovda/customview/MainActivity.kt
@@ -6,30 +6,16 @@ import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        counter1.setOnClickListener {
-            increment(it)
+        btnStart.setOnClickListener{
+            progress.setValue(100)
         }
-        counter2.setOnClickListener {
-            increment(it)
-        }
-        counter3.setOnClickListener {
-            increment(it)
-        }
-    }
-
-    private fun increment(view: View){
-        val indicatorView = view as IndicatorView
-        val max = indicatorView.getMaxValue()
-        val value = indicatorView.getValue()
-
-        if (value + 1 > max) {
-            indicatorView.setValue(0)
-        } else {
-            indicatorView.setValue(value + 1)
+        btnReset.setOnClickListener{
+            progress.setValue(0)
         }
     }
 }
